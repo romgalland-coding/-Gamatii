985e7f273a4338f64aa864aaf4b3f7919b6ab4cc
Theo Lorang <theo.lorang@gmail.com>
2026-06-01 16:16:21 +0200

    feat: routes and models creation

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
   belongs_to :user
+  has_many :list_games, dependent: :destroy
+  has_many :games, through: :list_games
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,4 @@
 Rails.application.routes.draw do
-  get "quizz_games/create"
-  get "quizz_games/destroy"
-  get "list_games/create"
-  get "list_games/destroy"
-  get "quizzs/index"
-  get "quizzs/show"
-  get "quizzs/new"
-  get "quizzs/create"
-  get "quizzs/edit"
-  get "quizzs/update"
-  get "quizzs/destroy"
-  get "lists/index"
-  get "lists/show"
-  get "lists/new"
-  get "lists/create"
-  get "lists/edit"
-  get "lists/update"
-  get "lists/destroy"
-  get "games/index"
-  get "games/show"
   devise_for :users
 
   resources :games, only: [:index, :show]
