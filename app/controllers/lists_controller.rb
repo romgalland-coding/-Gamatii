1718c871467209123e5fdae0222ebac80cb7ba3d
carlfarra <carlfarra@gmail.com>
2026-06-02 11:23:15 +0200

    added list methods and css components

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,22 +1,55 @@
 class ListsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :set_list, only: %i[show edit update destroy]
+
   def index
+    if current_user
+      @lists = current_user.lists.includes(:games).order(created_at: :desc)
+    else
+      @lists = List.includes(:games).order(created_at: :desc)
+    end
   end
 
   def show
+    @games = @list.games
   end
 
   def new
+    @list = List.new
   end
 
   def create
+    @list = current_user.lists.build(list_params)
+    if @list.save
+      redirect_to @list
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    if @list.update(list_params)
+      redirect_to @list
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
   end
 end
