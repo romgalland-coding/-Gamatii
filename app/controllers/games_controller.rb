5e1539fc7f2f0a74dfb410814eadc9f025d6c1e1
Romain Galland <rom.galland@gmail.com>
2026-06-02 15:17:53 +0200

    correctif

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,7 @@
 class GamesController < ApplicationController
   def show
-    authorize @games
     @game = Game.find(params[:id])
+    authorize @game
     return unless current_user
 
     @user_lists = current_user.lists.joins(:list_games).where(list_games: { game_id: @game.id })
