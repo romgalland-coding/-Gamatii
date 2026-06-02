6857a92ef64ce61117312492881294e00c30d2de
Theo Lorang <theo.lorang@gmail.com>
2026-06-02 11:13:30 +0200

    feat: add user devices selection on registration and edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:gamer_tag, platform: []])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:gamer_tag, platform: []])
+  end
 end
