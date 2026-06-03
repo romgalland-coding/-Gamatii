f397e58b0ee5257a4c6e75dcee68a1043a790091
Romain Galland <rom.galland@gmail.com>
2026-06-03 10:15:48 +0200

    migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_02_094618) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_03_081355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -28,6 +28,15 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_094618) do
     t.date "release_date"
     t.string "title"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "list_games", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "game_id", null: false
+    t.bigint "list_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_list_games_on_game_id"
+    t.index ["list_id"], name: "index_list_games_on_list_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -213,6 +222,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_02_094618) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "list_games", "games"
+  add_foreign_key "list_games", "lists"
   add_foreign_key "lists", "users"
   add_foreign_key "quiz_games", "games"
   add_foreign_key "quiz_games", "quizzes"
