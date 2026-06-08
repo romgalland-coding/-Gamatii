582a3cda64dc099c80a0b2fe95aa6bc045b8fe48
carlfarra <carlfarra@gmail.com>
2026-06-08 10:20:52 +0200

    added followers bio and avatar to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_08_081358) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.string "content_type"
+    t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "chats", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,6 +48,16 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "follows", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "followed_id", null: false
+    t.bigint "follower_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["followed_id"], name: "index_follows_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
   create_table "games", force: :cascade do |t|
@@ -47,7 +85,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
     t.bigint "list_id", null: false
     t.datetime "updated_at", null: false
     t.index ["game_id"], name: "index_list_games_on_game_id"
-    t.index ["list_id", "game_id"], name: "index_list_games_on_list_id_and_game_id", unique: true
     t.index ["list_id"], name: "index_list_games_on_list_id"
   end
 
@@ -57,6 +94,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["list_id"], name: "index_list_likes_on_list_id"
+    t.index ["user_id", "list_id"], name: "index_list_likes_on_user_id_and_list_id", unique: true
     t.index ["user_id"], name: "index_list_likes_on_user_id"
   end
 
@@ -251,6 +289,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.text "bio"
     t.datetime "created_at", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -264,7 +303,11 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_05_094026) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chats", "users"
+  add_foreign_key "follows", "users", column: "followed_id"
+  add_foreign_key "follows", "users", column: "follower_id"
   add_foreign_key "list_games", "games"
   add_foreign_key "list_games", "lists"
   add_foreign_key "list_likes", "lists"
